20cf2bcda3a0c87d37df087926e24607616a337e
duhast52 <65067705+duhast52@users.noreply.github.com>
2020-08-28 13:37:14 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,84 @@
+function ibg() {
+
+    $.each($('.ibg'), function(index, val) {
+        if ($(this).find('img').length > 0) {
+            $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+        }
+    });
+}
+
+ibg();
+
+$(document).ready(function() {
+    $('.icon-menu').click(function(event) {
+        $('.icon-menu').toggleClass('active');
+        $('.menu__body').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
+})
+
+
+$(function() {
+    $('.portfolio__toggles button').click(function() {
+        var get_id = this.id;
+        var get_current = $('.portfolio__posts .' + get_id);
+
+        $('.post').not(get_current).hide(500);
+        get_current.show(500);
+    });
+
+    $('#showall').click(function() {
+        $('.post').show(500);
+    });
+});
+
+if ($('.our-team__slider').length > 0) {
+    $('.our-team__slider').slick({
+        dots: true,
+        arrows: false,
+        accessibility: false,
+        slidesToShow: 1,
+        autoplaySpeed: 3000,
+        adaptiveHeight: true,
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {}
+        }]
+    });
+}
+
+if ($('.testimonials__bottom').length > 0) {
+    $('.testimonials__bottom').slick({
+        dots: true,
+        arrows: false,
+        accessibility: false,
+        slidesToShow: 1,
+        autoplaySpeed: 3000,
+        adaptiveHeight: true,
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {}
+        }]
+    });
+}
+
+if ($('.news__body').length > 0) {
+    $('.news__body').slick({
+        dots: true,
+        arrows: false,
+        accessibility: false,
+        slidesToShow: 1,
+        autoplaySpeed: 3000,
+        adaptiveHeight: true,
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {}
+        }]
+    });
+}
